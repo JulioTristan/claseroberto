584390ec1ca1d5824cf442aec4b28b1054d7789d
SaintBerry <robertom.95@hotmail.com>
2023-04-06 12:10:52 -0600

    Update Code

--- Clase02.js
+++ Clase02.js
@@ -1,18 +1,24 @@
 //Strings
-let input = "05:00:00AM";
+let input = "12:45:05PM";
 //que se vea formato 24 horas
 let hora = Number(input.slice(0, 2));
 let minutos = input.slice(3, 5);
 let segundos = input.slice(6, 8);
 if (input.slice(-2) === "PM") {
   hora = hora + 12;
-  hora=hora.toString()
   if (hora === 24) {
+    hora = "12";
+  } else {
+    hora = hora.toString();
+  }
+} else {
+  if (hora < 10) {
+    hora = hora.toString();
+    hora = `0${hora}`;
+  } else if (hora < 12) {
+    hora = hora.toString();
+  } else {
     hora = "00";
   }
-}
-else{
-    hora=hora.toString();
-    hora=`0${hora}`
 }
 console.log(`${hora}:${minutos}:${segundos}`);
